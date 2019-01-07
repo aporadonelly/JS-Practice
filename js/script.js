8a53528f4438915a2c8f73928a16f43d3c1245b2
nellyaporado <aporadonelly@gmail.com>
2019-01-07 16:43:49 +0800

    JS practice

---
+++ js/script.js
@@ -0,0 +1,99 @@
+// let btn = document.querySelector("#add");
+// btn.addEventListener('click', add);
+
+
+// let btn = document.querySelector("#btn");
+// btn.addEventListener('click', getInput);
+
+// function getInput (){
+// 	let result = document.querySelector("#result").value;
+
+// }
+
+
+
+// addtion
+
+// function addition(a, b) {
+//   return a + b;
+// }
+
+// let add = addition(1, 2);
+// console.log( add ); 
+
+
+
+// // subtraction
+// function subtraction(a, b) {
+//   return a - b;
+// }
+
+// let diff = subtraction(1, 2);
+// console.log( diff ); 
+
+
+// // multiplication
+// function multiplication(a, b) {
+//   return a * b;
+// }
+
+// let multiply = multiplication(1, 2);
+// console.log( multiply ); 
+
+
+// // division
+// function division(a, b) {
+//   return a * b;
+// }
+
+// let div = division(1, 2);
+// console.log( div ); 
+
+
+// modulo
+// function modulo(a, b) {
+//   return a % b;
+// }
+
+// let mod = modulo(1, 2);
+// console.log( mod ); 
+
+
+
+let mod = document.querySelector("#mod");
+mod.addEventListener('click', modulo);
+
+function modulo(){
+	let input1 = document.querySelector("#input1").value;
+	let input2 = document.querySelector("#input2").value;
+	console.log(input1 % input2);
+	// document.getElementById('#result').innerHTML="modulo()"	;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let btn = document.querySelector("#btn");
+// btn.addEventListener('click', getInput);
+
+// function getInput (){
+// 	let input1 = document.querySelector("#input1").value;
+
+// }
+// 	if (input1 > 10){
+// 		alert("Greater than 10");
+// 	}else {
+// 		console.log("Lesser than 10");
+// 	}
+
